bfe4e613024db939f699848ae15f43efcb7d8c36
Ruwan <ruwanliyanagegalle1994@gmail.com>
2025-01-08 21:37:51 +0530

    minor modification

--- src/main/java/com/bodyguard/mybank/userpass/repository/GoldRepository.java
+++ src/main/java/com/bodyguard/mybank/userpass/repository/GoldRepository.java
@@ -4,6 +4,10 @@ import com.bodyguard.mybank.userpass.entity.GoldEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+/**
+ * Repository for GoldEntity
+ */
+
 @Repository
 public interface GoldRepository extends JpaRepository<GoldEntity, Integer> {
 }
--- src/main/java/com/bodyguard/mybank/userpass/repository/LoanRepository.java
+++ src/main/java/com/bodyguard/mybank/userpass/repository/LoanRepository.java
@@ -4,6 +4,9 @@ import com.bodyguard.mybank.userpass.entity.LoanEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+/**
+ * Repository for LoanEntity
+ */
 @Repository
 public interface LoanRepository extends JpaRepository<LoanEntity, Integer> {
 }
